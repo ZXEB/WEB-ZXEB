0b5b80bddd83b510b696f8393a85f5540c6fe7a6
ZXEB <108905608+ZXEB@users.noreply.github.com>
2022-12-03 14:41:36 +0800

    Create jjj.js

---
+++ jjj.js
@@ -0,0 +1,8 @@
+Calendar cal=Calendar.getInstance();      
+ int y=cal.get(Calendar.YEAR);      
+ int m=cal.get(Calendar.MONTH);      
+ int d=cal.get(Calendar.DATE);      
+ int h=cal.get(Calendar.HOUR_OF_DAY);      
+ int mi=cal.get(Calendar.MINUTE);      
+ int s=cal.get(Calendar.SECOND);      
+ System.out.println("现在时刻是"+y+"年"+m+"月"+d+"日"+h+"时"+mi+"分"+s+"秒");
